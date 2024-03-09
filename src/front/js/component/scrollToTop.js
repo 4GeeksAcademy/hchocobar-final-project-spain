84b1779bc556c2b2d44eb75302578d54a610fa18
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-03-09 03:51:43 +0000

    spaces

--- src/front/js/component/scrollToTop.js
+++ src/front/js/component/scrollToTop.js
@@ -3,21 +3,21 @@ import PropTypes from "prop-types";
 
 
 class ScrollToTop extends React.Component {
-	componentDidUpdate(prevProps) {
-		if (this.props.location !== prevProps.location) {
-			window.scrollTo(0, 0);
-		}
-	}
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
 
-	render() {
-		return this.props.children;
-	}
+  render() {
+    return this.props.children;
+  }
 }
 
 export default ScrollToTop;
 
 
 ScrollToTop.propTypes = {
-	location: PropTypes.object,
-	children: PropTypes.any
+  location: PropTypes.object,
+  children: PropTypes.any
 };
